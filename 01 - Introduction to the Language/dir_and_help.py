2d54955d23761ce37036f8f73d5a225da7f8a59a
Tiago Gonçalves <tiago.tsg@gmail.com>
2024-10-29 13:04:40 -0300

    Introduction to the Language

---
+++ 01 - Introduction to the Language/dir_and_help.py
@@ -0,0 +1,36 @@
+# Comandos da console Python
+
+# python --> Acessa o console na versão do Python padrão do Sistema.
+# Obs.: Via terminal da console da IDE, ele acessa a versão corrente (.venv).
+
+# python3.8 --> Acessa a console na versão informada.
+
+
+# dir --> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+"""
+
+dir(tipo de dados/variável)
+
+
+exemplo: dir("Vasco da Gama") --> Retorna tudo que é possível fazer com uma string (funções).
+
+exemplo: dir(4) --> Retorna tudo que é possível fazer com um dado do tipo numérico (funções).
+
+"""
+
+# help --> Apresenta a documentação de como utilizar os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+"""
+
+help(tipo de dado/variável)
+
+exemplo: help("Vasco da Gama".lower) --> Abre a documentação da função "lower".
+
+exemplo:
+
+num = 4
+
+help(num.to_bytes) --> Abre a documentação da função "to_bytes".
+
+"""
